e2bc20620e5061e265c6aa10076b29a3279140d4
SDargarh <sdargarh26@gmail.com>
2025-03-13 09:19:22 +0000

    aerodrome adapter basic and concentrated pools

---
+++ src/adapters/aerodrome/AerodromeV2.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {AerodromeBaseAdapter} from "./AerodromeBaseAdapter.sol";
+import { ISharePriceRouter } from "../../interfaces/ISharePriceRouter.sol";
+import { IAerodromeV2Pool } from "../../interfaces/aerodrome/IAerodromeV2Pool.sol";
+
+import {console2} from "forge-std/console2.sol";
+
+import { FixedPointMathLib } from "@solady/utils/FixedPointMathLib.sol";
+
+contract AerodromeV2Adapter is AerodromeBaseAdapter {
+    using FixedPointMathLib for uint256;
+    using FixedPointMathLib for uint160;
+
+    /// CONSTRUCTOR ///
+
+    constructor(
+        address _admin,
+        address _oracle,
+        address _oracleRouter
+    )
+    AerodromeBaseAdapter(_admin, _oracle, _oracleRouter) {}
+
+    /// EXTERNAL FUNCTIONS ///
+
+    /// @notice Retrieves the price of `asset` from Aerodrom pool
+    /// @dev Price is returned in USD or ETH depending on 'inUSD' parameter.
+    /// @param asset The address of the asset for which the price is needed.
+    /// @param inUSD A boolean to determine if the price should be returned in
+    ///              USD or not.
+    /// @return pData A structure containing the price, error status,
+    ///                         and the quote format of the price.
+    function getPrice(
+        address asset,
+        bool inUSD
+    ) external view virtual override returns (ISharePriceRouter.PriceReturnData memory pData) {
+        // Validate we support pricing `asset`.
+        if (!isSupportedAsset[asset]) {
+            revert AerodromeAdapter__AssetIsNotSupported();
+        }
+
+        AdapterData memory data = adapterData[asset];
+
+        // Get underlying token prices.
+        
+        (uint256 basePrice, bool errorCode) = oracleRouter.getPrice(
+            data.baseToken, inUSD
+        );
+        console2.log("### ~ AerodromeV2.sol:50 ~ )externalviewvirtualoverridereturns ~ basePrice:", basePrice);
+        if (errorCode) {
+            pData.hadError = true;
+            return pData;
+        }
+
+        (uint160 sqrtPriceX96,,,,,) = IAerodromeV2Pool(data.pool).slot0();
+        console2.log("### ~ AerodromeV2.sol:51 ~ )externalviewvirtualoverridereturns ~ sqrtPriceX96:", sqrtPriceX96);
+
+        //  Convert sqrtPriceX96 to sqrtPrice
+        uint256 sqrtPrice = FixedPointMathLib.fullMulDiv(sqrtPriceX96, 1e18, 2**96);
+        console2.log("### ~ AerodromeV2.sol:61 ~ )externalviewvirtualoverridereturns ~ sqrtPrice:", sqrtPrice);
+
+        // Compute USDC per cbBTC (price ratio)
+        uint256 priceUSDCPerCbBTC = sqrtPrice.fullMulDiv(sqrtPrice, 1e18);
+
+        // Compute cbBTC per USDC
+        uint256 priceCbBTCPerUSDC = uint256(1e18).fullMulDiv(1e18, priceUSDCPerCbBTC);
+
+        console2.log("### ~ AerodromeV2.sol:66 ~ )externalviewvirtualoverridereturns ~ priceCbBTCPerUSDC:", priceCbBTCPerUSDC);
+
+        uint256 scaleFactor = data.quoteTokenDecimals > data.baseTokenDecimals 
+            ? 10 ** (data.quoteTokenDecimals - data.baseTokenDecimals) 
+            : 1e18 / (10 ** (data.baseTokenDecimals - data.quoteTokenDecimals));
+
+        uint256 price = priceCbBTCPerUSDC * scaleFactor;
+        console2.log("### ~ AerodromeV2.sol:70 ~ )externalviewvirtualoverridereturns ~ price:", price);
+
+        // uint price = ( 10 ** (data.baseTokenDecimals - data.quoteTokenDecimals)) / sqrtPriceX96;
+        // console2.log("### ~ AerodromeV2.sol:56 ~ )externalviewvirtualoverridereturns ~ price:", price);
+
+
+        // uint256 price = IAerodromeV1Pool(data.pool).getAmountOut(uint256(1 * (10** data.quoteTokenDecimals)), asset);
+
+        price = (price * basePrice) / WAD;        
+        console2.log("### ~ AerodromeV2.sol:62 ~ )externalviewvirtualoverridereturns ~ price:", price);
+        if (_checkOracleOverflow(price)) {
+            pData.hadError = true;
+            return pData;
+        }
+
+        pData.inUSD = inUSD;
+        pData.price = uint240(price);
+    }
+
+    /// @notice Helper function for pricing support for `asset`,
+    ///         an lp token for a Univ2 style stable liquidity pool.
+    /// @dev Should be called before `OracleRouter:addAssetPriceFeed`
+    ///      is called.
+    /// @param asset The address of the lp token to add pricing support for.
+    function addAsset(
+        address asset,
+        AdapterData memory data
+    ) public override {
+
+        _checkOraclePermissions();
+
+        if (!isAeroPool(data.pool)) {
+            revert AerodromeAdapter__InvalidPoolAddress();
+        }
+
+        super.addAsset(asset, data);
+    }
+
+    function isAeroPool(address pool) public view returns (bool) {
+        try IAerodromeV2Pool(pool).token0() returns (address) {
+            return true;
+        } catch {
+            return false;
+        }
+    }
+}
---
+++ src/interfaces/aerodrome/IAerodromeV1Pool.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.17;
+
+interface IAerodromeV1Pool {
+
+    /// @notice Returns [token0, token1]
+    function tokens() external view returns (address, address);
+    
+    /// @notice Address of token in the pool with the lower address value
+    function token0() external view returns (address);
+
+    /// @notice Address of token in the poool with the higher address value
+    function token1() external view returns (address);
+
+    /// @notice Provides twap price with user configured granularity, up to the full window size
+    /// @param tokenIn .
+    /// @param amountIn .
+    /// @param granularity .
+    /// @return amountOut .
+    function quote(address tokenIn, uint256 amountIn, uint256 granularity) external view returns (uint256 amountOut);
+
+    /// @notice Returns a memory set of TWAP prices
+    ///         Same as calling sample(tokenIn, amountIn, points, 1)
+    /// @param tokenIn .
+    /// @param amountIn .
+    /// @param points Number of points to return
+    /// @return Array of TWAP prices
+    function prices(address tokenIn, uint256 amountIn, uint256 points) external view returns (uint256[] memory);
+
+    /// @notice Same as prices with with an additional window argument.
+    ///         Window = 2 means 2 * 30min (or 1 hr) between observations
+    /// @param tokenIn .
+    /// @param amountIn .
+    /// @param points .
+    /// @param window .
+    /// @return Array of TWAP prices
+    function sample(
+        address tokenIn,
+        uint256 amountIn,
+        uint256 points,
+        uint256 window
+    ) external view returns (uint256[] memory);
+
+    
+    /// @notice Get the amount of tokenOut given the amount of tokenIn
+    /// @param amountIn Amount of token in
+    /// @param tokenIn  Address of token
+    /// @return Amount out
+    function getAmountOut(uint256 amountIn, address tokenIn) external view returns (uint256);
+}
